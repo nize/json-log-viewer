ca8934976ed788e82b195f917d6f6028fa2c7b21
Nils Zetterlund <nils.zetterlund@gmail.com>
2024-07-05 15:13:08 +0200

    Removed some console printing

--- log_viewer.py
+++ log_viewer.py
@@ -54,21 +54,21 @@ def process_line(line, event_queue, cleartext, password, program_id, run_id):
         try:
             decrypted_data = decrypt_and_decompress(line.strip(), password)
             event = json.loads(decrypted_data)
-            print("Processed event:", event) 
+            #print("Processed event:", event) 
         except Exception as e:
             print(f"Failed to decrypt and decompress log file: {e}")
             return
     else:
         try:
             event = json.loads(line.strip())
-            print("Processed event:", event)
+            #print("Processed event:", event)
         except json.JSONDecodeError as e:
             print(f"Failed to decode line: {e}")
             return
 
     if should_include_event(event, program_id, run_id):
         event_queue.put(event)  # Add the event to the queue
-        print("Event added to queue:", event)
+        #print("Event added to queue:", event)
 
 def read_last_lines(file_path, n=100):
     """Read the last n lines of a file without loading the whole file into memory."""
@@ -147,7 +147,7 @@ def display_events(stdscr, event_queue):
             while True:  # Drain the queue if there are multiple entries
                 event = event_queue.get_nowait()
                 event_list.insert(0, event)
-                print("Event displayed:", event)
+                #print("Event displayed:", event)
                 if current_row > 0:
                     new_data_available = True  # Set flag if not at the top
                 event_queue.task_done()
@@ -281,7 +281,7 @@ def main():
     event_queue = queue.Queue()
     log_thread = threading.Thread(target=parse_log_file, args=(log_file_path, event_queue, cleartext, password, program_id, run_id))
     log_thread.daemon = True
-    print("Starting thread...")
+    #print("Starting thread...")
     log_thread.start()
     log_thread.join() # For testing purposes
 
